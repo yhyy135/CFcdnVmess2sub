4e84966e6e782174e6b83b204944637b04d7ccea
yhyy135 <yhyy135@users.noreply.github.com>
2026-07-23 02:13:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,10 @@ let addressesapi = [
 	// 'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
 	'https://addressesapi.090227.xyz/ip.164746.xyz',
-	'https://ipdb.api.030101.xyz/?type=bestcf&country=true',
+	//'https://ipdb.api.030101.xyz/?type=bestcf&country=true',
+	'https://cf.090227.xyz/ct',
+	'https://cf.090227.xyz/cmcc',
+	'https://cf.090227.xyz/cu',
 	// 'https://ipdb.api.030101.xyz/?type=bestproxy&country=true'
 ];
 
